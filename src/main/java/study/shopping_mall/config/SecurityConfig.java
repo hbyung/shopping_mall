fbcd984d3bee02f787fc919145fc26b2b99c7d40
hbyung <148026619+hbyung@users.noreply.github.com>
2024-04-17 03:41:20 +0900

    Update SecurityConfig.java

--- src/main/java/study/shopping_mall/config/SecurityConfig.java
+++ src/main/java/study/shopping_mall/config/SecurityConfig.java
@@ -24,24 +24,24 @@ import study.shopping_mall.service.CustomOAuth2UserService;
 
 
 @Configuration
-// @EnableWebSecurity
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-   private final AuthenticationConfiguration authenticationConfiguration;
-   private final CustomOAuth2UserService customOAuth2UserService;
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final CustomOAuth2UserService customOAuth2UserService;
 
-   @Bean
-   public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
 
-       return new BCryptPasswordEncoder();
-   }
+        return new BCryptPasswordEncoder();
+    }
 
-   @Bean
-   public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
 
-       return configuration.getAuthenticationManager();
-   }
+        return configuration.getAuthenticationManager();
+    }
 
 //    @Bean
 //    public AccessDeniedHandler accessDeniedHandler() {
@@ -65,110 +65,110 @@ public class SecurityConfig {
 //        };
 //    }
 
-   @Bean
-   public RoleHierarchy roleHierarchy() {
+    @Bean
+    public RoleHierarchy roleHierarchy() {
 
-       RoleHierarchyImpl hierarchy = new RoleHierarchyImpl();
+        RoleHierarchyImpl hierarchy = new RoleHierarchyImpl();
 
-       hierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER\n" +
-               "ROLE_USER > ROLE_BLACK");
+        hierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER\n" +
+                "ROLE_USER > ROLE_BLACK");
 
-       return hierarchy;
-   }
+        return hierarchy;
+    }
 
-   @Bean
-   public SessionRegistry sessionRegistry() {
-       return new SessionRegistryImpl();
-   }// Register HttpSessionEventPublisher
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }// Register HttpSessionEventPublisher
 
-   @Bean
-   public static ServletListenerRegistrationBean httpSessionEventPublisher() {
-       return new ServletListenerRegistrationBean(new HttpSessionEventPublisher());
-   }
+    @Bean
+    public static ServletListenerRegistrationBean httpSessionEventPublisher() {
+        return new ServletListenerRegistrationBean(new HttpSessionEventPublisher());
+    }
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-//        //csrf disable
+        //csrf disable
+        http
+                .csrf((auth) -> auth.disable());
+
+
+        //From 로그인 방식 disable
+        http
+                .formLogin((auth) -> auth.loginPage("/login")
+                        .loginProcessingUrl("/loginProc")
+                        .usernameParameter("email")
+                        .passwordParameter("password")
+                        .defaultSuccessUrl("/")
+                        .permitAll()
+                );
+
+        http
+                .logout((logout) ->
+                        logout.logoutUrl("/logout")
+                                .logoutSuccessUrl("/")
+                );
+
+        http
+                .exceptionHandling((handling) ->
+                        handling
+//                                .authenticationEntryPoint(authenticationEntryPoint())
+//                                .accessDeniedHandler(accessDeniedHandler())
+                                .accessDeniedPage("/errorPage")
+                );
+
+
+        //http basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
+
+
+        //중복로그인
+        http
+                .sessionManagement((auth) -> auth
+                        .invalidSessionUrl("/")
+                        .maximumSessions(2)
+                            .maxSessionsPreventsLogin(false)
+                             .expiredUrl("/login")
+                );
+
+        //세션 고정 보안
+        http
+                .sessionManagement((auth) -> auth
+                        .sessionFixation().changeSessionId());
+
+
+        http
+                .oauth2Login((oauth2) -> oauth2
+                                .loginPage("/login")
+                                .userInfoEndpoint((userInfoEndpointConfig ->
+                                        userInfoEndpointConfig.userService(customOAuth2UserService)))
+                );
+
+
+        //경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/login/**", "/", "/join", "/error","/oauth2/**", "Member/**", "item/**", "ItemList" ).permitAll()
+                        .requestMatchers("/css/**","/js/**", "/assets/**", "/img/**", "/static/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/cart/**","/order", "/orders", "/orders/**","user/**").hasRole("USER")
+                        .anyRequest().authenticated());
+
+
 //        http
-//                .csrf((auth) -> auth.disable());
-
-
-//        //From 로그인 방식 disable
+//                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
+//
 //        http
-//                .formLogin((auth) -> auth.loginPage("/login")
-//                        .loginProcessingUrl("/loginProc")
-//                        .usernameParameter("email")
-//                        .passwordParameter("password")
-//                        .defaultSuccessUrl("/")
-//                        .permitAll()
-//                );
+//                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
+
+        //세션 설정
 //        http
-//                .logout((logout) ->
-//                        logout.logoutUrl("/logout")
-//                                .logoutSuccessUrl("/")
-//                );
+//                .sessionManagement((session) -> session
+//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-//        http
-//                .exceptionHandling((handling) ->
-//                        handling
-// //                                .authenticationEntryPoint(authenticationEntryPoint())
-// //                                .accessDeniedHandler(accessDeniedHandler())
-//                                .accessDeniedPage("/errorPage")
-//                );
-
-
-//        //http basic 인증 방식 disable
-//        http
-//                .httpBasic((auth) -> auth.disable());
-
-
-//        //중복로그인
-//        http
-//                .sessionManagement((auth) -> auth
-//                        .invalidSessionUrl("/")
-//                        .maximumSessions(2)
-//                            .maxSessionsPreventsLogin(false)
-//                             .expiredUrl("/login")
-//                );
-
-//        //세션 고정 보안
-//        http
-//                .sessionManagement((auth) -> auth
-//                        .sessionFixation().changeSessionId());
-
-
-//        http
-//                .oauth2Login((oauth2) -> oauth2
-//                                .loginPage("/login")
-//                                .userInfoEndpoint((userInfoEndpointConfig ->
-//                                        userInfoEndpointConfig.userService(customOAuth2UserService)))
-//                );
-
-
-//        //경로별 인가 작업
-//        http
-//                .authorizeHttpRequests((auth) -> auth
-//                        .requestMatchers("/login/**", "/", "/join", "/error","/oauth2/**", "Member/**", "item/**", "ItemList" ).permitAll()
-//                        .requestMatchers("/css/**","/js/**", "/assets/**", "/img/**", "/static/**").permitAll()
-//                        .requestMatchers("/admin/**").hasRole("ADMIN")
-//                        .requestMatchers("/cart/**","/order", "/orders", "/orders/**","user/**").hasRole("USER")
-//                        .anyRequest().authenticated());
-
-
-// //        http
-// //                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
-// //
-// //        http
-// //                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
-
-
-//        //세션 설정
-// //        http
-// //                .sessionManagement((session) -> session
-// //                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-//        return http.build();
-   }
+        return http.build();
+    }
 }
