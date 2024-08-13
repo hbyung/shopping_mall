7e0242e65f457cd6134dd40748e20aa46c9257aa
byung hhwan <tjsdnd627@gmail.com>
2024-08-13 20:21:09 +0900

    cache 및 redisrepository 수정

---
+++ src/main/java/study/shopping_mall/config/CachingConfig.java
@@ -0,0 +1,26 @@
+package study.shopping_mall.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+
+    @EnableCaching
+    @Configuration
+    public class CachingConfig {
+
+        @Bean
+        public CacheManager cacheManager() {
+            ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager();
+            cacheManager.setAllowNullValues(false);
+            cacheManager.setCacheNames(List.of("food"));
+            cacheManager.setCacheNames(List.of("CartAll"));
+            return cacheManager;
+        }
+    }
+
+
